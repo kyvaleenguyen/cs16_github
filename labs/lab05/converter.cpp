6fd651c15fed1da01e066f5c6ab88a8639818fc0
Kyvalee Nguyen <kyvaleenguyen@ucsb.edu>
2026-02-04 02:04:38 -0800

    added lab 5 programs

---
+++ labs/lab05/converter.cpp
@@ -0,0 +1,25 @@
+#include <iostream> // for cin, cout, endl
+#include <cstdlib>  // for exit()
+#include <cmath>    // for pow() function ONLY
+// YOU ARE NOT ALLOWED TO INCLUDE ANY OTHER LIBRARY!!! (zero grade penalty)
+using namespace std;
+
+// FUNCTION DECLARATIONS: YOU MUST DEFINE AND USE THESE (do not remove):
+//      You can declare/define other functions if you like as well,
+//      but you must use the 2 functions below.
+int bin2d(string binstring);
+string dec2bh(string sdec, char bh);
+
+int main ( int argc, char *argv[] ) {
+
+    // MISSING CODE HERE (you can remove these comments)
+    // Check to see if the command-line arguments are being used correctly
+    // Perform the conversion by calling the appropriate function
+    // What happens next?!!?
+
+    return 0;
+}
+
+// MISSING FUNCTION DEFINITIONS HERE 
+// Make sure you have Pre-Conditions and Post-Conditions defined for each function you define here!
+// (you can remove these comments)
---
+++ labs/lab05/vectorSplit.cpp
@@ -0,0 +1,24 @@
+#include <iostream> // for cin, cout
+#include <vector>   // for vector
+#include <string>   // for getline()
+// YOU ARE NOT ALLOWED TO INCLUDE ANY OTHER LIBRARY!!! (zero grade penalty)
+using namespace std;
+
+// FUNCTION DECLARATION: YOU MUST DEFINE AND USE THIS (do not remove):
+//      You can declare/define other functions if you like as well,
+//      but you must use function split() below.
+vector<string> split(string target, string delimiter);
+
+int main() {
+
+    // MISSING CODE HERE (you can remove these comments)
+    // Get target string from user, get delimiter string from user
+    // Call function split()
+    // What happens next?!!?
+
+    return 0;
+}
+
+// MISSING FUNCTION DEFINITIONS HERE 
+// Make sure you have Pre-Conditions and Post-Conditions defined for each function you define here!
+// (you can remove these comments)
