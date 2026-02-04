e83db118999ae1f6ff639a9088a4afebf436f4d9
Kyvalee Nguyen <kyvaleenguyen@ucsb.edu>
2026-02-03 17:58:25 -0800

    completed stats.cpp

--- labs/lab04/stats.cpp
+++ labs/lab04/stats.cpp
@@ -1,118 +1,116 @@
-#include <iostream> // for cout, cin, endl
-#include <cmath>    // for pow -- the only one you're allowed to use from this lib
-#include <iomanip>  // for setprecision, setw -- the only ones you're allowed to use from this lib
+#include <iostream>
+#include <iomanip> // Required for setw, setprecision
+#include <cmath>   // Required for pow
+
 using namespace std;
 
-// FUNCTION DECLARATIONS: YOU MUST USE THESE (do not remove):
-void bubbleSort(int *array, int size);
-double average(int *array, int size);
-double median(int* array, int size);
-double stddev(int* array, int size);
-
-// This is bubbleSort - it is given for you to use.
-// Pre-Condition:: It takes in a dynamic int array and its size
-// Post-Condition: It sorts the array in ascending order of its values
-void bubbleSort(int *array, int size) {
-  int temp;
-  for (int i = size-1; i >= 0; i--) {
-    for (int j = 1; j <= i; j++) {
-      if (array[j-1] > array[j]) {
-        temp = array[j-1];
-        array[j-1] = array[j];
-        array[j] = temp;
-      } // if
-    } // for j
-  } // for i
-}
-
-int avr = 0;
-int sum = 0;
-// average of array
-double average(int *array, int size) {
-  for (int i = 0; i < size; i++) {
-    sum += array[i];
-  }
-  avr = sum / 2;
-  cout << "Average = " << avr << endl;
-  return 0;
-}
-
-int med = 0;
-// median of array
-double median(int* array, int size) {
-  if (size % 2 == 0) {
-    med = array[size / 2];
-  } else {
-    med = array[(size / 2) + 1];
-  }
-  cout << "Median = " << med << endl;
-  return 0;
-}
-
-// standard deviation of array
-double size_ratio = 0.0;
-double stddev(int* array, int size) {
-  size_ratio = 1.0/(size - 1.0);
-  double sqr_dif_sums = 0.0;
-  for (int i = 0; i < size; i++) {
-      sqr_dif_sums += pow(array[i] - avr, 2);
-  }
-  double std_dev = size_ratio * sqr_dif_sums;
-
-  // Trace size
-  cout << "size = " << size - 1 << endl;
-
-  // Trace sqr dif sums
-  cout << "sqr_dif_sums = " << sqr_dif_sums << endl;
-
-  // Trace size_ratio
-  cout << "size_ratio = " << size_ratio << endl;
-
-  cout << "Standard Deviation = " << std_dev << endl;
-  return 0;
-}
+// Function declarations
+void bubbleSort(int *, int);
+double findAverage(int *, int);
+double findMedian(int *, int);
+double findStdDev(int *, int, double);
 
 int main() {
-  // Sets the printing of floating-point numbers
-  // to show only 2 places after the decimal point
+  // Set precision
   cout << fixed << showpoint;
   cout << setprecision(2);
 
-  // MISSING CODE HERE (you can remove these comments)
-// Get the number of grades to be entered from user
-  // Create a dynamic int array based on this information
-  // Get the user to enter all the grades
-  // Print results while calling the appropriate functions
-cout << "Enter number of grades : ";
-int grade_amount;
-cin >> grade_amount;
-cout << "Enter grades (each on a new line)" << endl;
-int size = grade_amount;
-for (int i = 0; i < size; i++) {
-  int grade;
-  cin >> grade;
-  int grades[] =+ grade;
-  cout << grades;
-}
+  // Get user input for grade amount
+  cout << "Enter number of grades: ";
+  int grade_amount;
+  cin >> grade_amount;
+  if (grade_amount < 1) {
+    cerr << "Error!" << endl;
+    exit(1);
+  }
+  
+  // Dynamic array
+  int *grades_list = new int[grade_amount];
 
-  // Test case: print grade_list array
+  // Get input for grades
+  cout << "Enter grades (each on a new line):" << endl;
+  for (int i = 0; i < grade_amount; i++) {
+    cin >> grades_list[i];
+  }
 
-  // Test case
-  int numbers[] = {5, 9, 8, 11, 35, 22, 30, 19, 40};
-  int nl_size = 10;
+  // Sort array
+  bubbleSort(grades_list, grade_amount);
 
-  //Trace avr var
-  cout << "Avr var is equal to: " << avr << endl;
-  // average of array
-  average(numbers, nl_size);
+  // Function calls
+  double average = findAverage(grades_list, grade_amount);
+  double median = findMedian(grades_list, grade_amount);
+  double stddev = findStdDev(grades_list, grade_amount, average);
 
-  // median of array
-  median(numbers, nl_size);
+  // Print statistics
+  cout << "Here are some statistics:" << endl;
+  cout << setw(9) << "Average: " << average << endl;
+  cout << setw(9) << "Median: " << median << endl;
+  cout << setw(9) << "StdDev: " << stddev << endl;
 
-  // standard deviation of array
-  stddev(numbers, nl_size);
+  // Eliminate dynamic array
+  delete[] grades_list;
+  grades_list = nullptr;
 
   return 0;
 }
 
-// MISSING FUNCTION DEFINITIONS HERE (you can remove these comments)
+// Function Definitions
+void bubbleSort(int *array, int size) {
+  bool swapped = true;
+  int j = 0;
+  int temp;
+
+  while (swapped) {
+    swapped = false;
+    j++;
+    for (int i = 0; i < size - j; i++) {
+      if (array[i] > array[i + 1]) {
+        temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  }
+}
+
+// Add all values in array and divide by number of values in array
+double findAverage(int *array, int size) {
+  double sum = 0.0;
+  for (int i = 0; i < size; i++) {
+    sum += array[i];
+  }
+  return sum / size;
+}
+
+// Find middle value in sorted array
+double findMedian(int *array, int size) {
+    if (size % 2 == 0) {
+      // Average of middle two is average
+      int mid1_index = size / 2 - 1;
+      int mid2_index = size / 2;
+      return (array[mid1_index] + array[mid2_index]) / 2.0;
+    } else {
+      // Middle one is average
+      return static_cast<double>(array[size / 2]);
+    }
+}
+
+// Find standard deviation using provided stddev formula
+double findStdDev(int *array, int size, double average) {
+    double sum_of_squares = 0.0;
+    for (int i = 0; i < size; i++) {
+      // Pow() function from <cmath>
+      sum_of_squares += pow(array[i] - average, 2);        
+    }
+
+
+
+    // Final standard deviation
+    if (size == 1) {
+      double std_dev = 0.0;
+      return std_dev;
+    }
+    double std_dev = sqrt((1.0/(size - 1)) * sum_of_squares);
+    return std_dev;
+}
