2787d90b5e1720dd91abdc3cc8c482c4a5519141
Kyvalee Nguyen <kyvaleenguyen@ucsb.edu>
2026-02-04 23:11:18 -0800

    completed vectorSplit

--- labs/lab05/vectorSplit.cpp
+++ labs/lab05/vectorSplit.cpp
@@ -1,59 +1,89 @@
-#include <iostream> // for cin, cout
-#include <vector>   // for vector
-#include <string>   // for getline()
-// YOU ARE NOT ALLOWED TO INCLUDE ANY OTHER LIBRARY!!! (zero grade penalty)
+#include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
-// FUNCTION DECLARATION: YOU MUST DEFINE AND USE THIS (do not remove):
-//      You can declare/define other functions if you like as well,
-//      but you must use function split() below.
+// Function declaration
 vector<string> split(string target, string delimiter);
 
-int main() {
-    // Get target string from user, get delimiter string from user
-    // Call function split()
-    // What happens next?!!?
-    /*string a = "bun's";
-    string delim = "'";
-    split(a, delim);*/
 
-
-    return 0;
-}
-
-// MISSING FUNCTION DEFINITIONS HERE 
-// Make sure you have Pre-Conditions and Post-Conditions defined for each function you define here!
-// (you can remove these comments)
-
-vector<string> split(string target, string delimiter) {
-    // Function definition
+int main () {
+    // Experimental vector<int> (and other vectors)
 
     // Get string from user
     cout << "Enter string to split:" << endl;
+    string target;
     getline(cin, target);
+
+    //cout << target[3] << endl;
+
+    /*string s = "s";
+    string t = "s";
+    cout << ( t == s) << endl;*/
 
     // Enter string delimiter
     cout << "Enter delimiter string" << endl;
+    string delimiter;
     getline(cin, delimiter);
     if (delimiter.size() > 1) {
         exit(1);
     }
 
-    // Form substrings
-    vector<string> sample;
-    for (int i = 0; i < target.size(); i++) {
-        if (target.find(delimiter)) {
-            vector<int> delimiter_indexes[i] = i;
-        }
+    // Keep in mind that if the statement below isn't true and runs the program will crash
+    /*cout << (target[0] == delimiter[0]) << endl;*/
 
-
-        if (target[i] == target.find(delimiter, i)) {
-            sample.push_back(target.substr(i, -((size-1)-i)));
+    // Sort substrings
+    /*for (int i = 0; i < target.size(); i++) {
+            int delimiter_position = target.find(delimiter);
+            cout << delimiter_position << endl;
         }
-    }
+    */
 
     // Print substrings
-    cout << "The substrings are:" << endl;
 
-    return sample;
+    // If no substrings
+    if (target.find(delimiter) == string::npos) {
+            cout << "No substrings." << endl;
+    }
+
+    // If substrings
+    if (target.find(delimiter) != string::npos) {
+        // Create substrings vector and word string to store substrings in and add to substrings vector
+        vector<string> substrings;
+        string word;
+        // Control size of substrings vector to save memory
+        substrings.reserve(target.size());
+        // Integer to set start of target index for substrings to 0
+        int start = 0;
+        for (int i = 0; i < target.size(); i++) {
+            // If delimiter found at index i in target and if thw substr size is greater than 0
+            if ((target[i] == delimiter[0]) && ((i-start)>=0)) {
+                word = target.substr(start, i - start);
+                if (word != "") {
+                    substrings.push_back(word);
+                }
+                // Change start index if delimiter found
+                start = i + 1;
+            }
+        }
+        // Get last substring after last delimiter
+        if (word != "") {
+            substrings.push_back(target.substr(start));
+        }
+
+        // Print substrings
+        cout << "The substrings are: ";
+        for (int i = 0; i < substrings.size(); i++) {
+            cout << "\"" << substrings[i] << "\"";
+            // Print out a comma if the index in substrings vector is not the last index of the vector
+            if ((i < substrings.size() - 1)) {
+                cout << ", ";
+            }
+        }
+        cout << endl;
+    }
+
+
+    return 0;
 }
