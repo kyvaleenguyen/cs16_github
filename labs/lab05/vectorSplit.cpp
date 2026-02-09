fe9444e019549976680c5d8e491b6e5fb82a4666
Kyvalee Nguyen <kyvaleenguyen@ucsb.edu>
2026-02-09 15:38:29 -0800

    fixed vectorSplit function according to lab instructions

--- labs/lab05/vectorSplit.cpp
+++ labs/lab05/vectorSplit.cpp
@@ -9,15 +9,25 @@ vector<string> split(string target, string delimiter);
 
 
 int main () {
+    // Split function call
+    string t, d;
+    split(t, d);
+    
+    return 0;
+}
+
+// Split function definition
+vector<string> split(string target, string delimiter) {
+    // Variable declarations
+    vector<string> substrings;
+    string word;
 
     // Get string from user
     cout << "Enter string to split:" << endl;
-    string target;
     getline(cin, target);
 
     // Enter string delimiter
-    cout << "Enter delimiter string" << endl;
-    string delimiter;
+    cout << "Enter delimiter string:" << endl;
     getline(cin, delimiter);
     // If delimiter entered has a character value of more than one, exit the program
     if (delimiter.size() > 1) {
@@ -30,11 +40,9 @@ int main () {
         cout << "No substrings." << endl;
     }
 
-    // If substrings
+    // If there are substrings
     if (target.find(delimiter) != string::npos) {
         // Create substrings vector and word string to store substrings in and add to substrings vector
-        vector<string> substrings;
-        string word;
         // Control size of substrings vector to save memory
         substrings.reserve(target.size());
         // Integer to set start of target index for substrings to 0
@@ -44,6 +52,11 @@ int main () {
             if ((target[i] == delimiter[0]) && ((i-start)>=0)) {
                 // If the word found is not an empty string, add it to the substrings vector
                 word = target.substr(start, i - start);
+                word.insert(0, "\"");
+                word.insert(word.size(), "\"");
+                // Insert comma after word found
+                word.insert(word.size(), ", ");
+                // If the word is not an empty string, add it to the substrings string vector
                 if (word != "") {
                     substrings.push_back(word);
                 }
@@ -51,23 +64,20 @@ int main () {
                 start = i + 1;
             }
         }
-        // Get last substring after last delimiter
+        // Get last substring after last delimiter & make sure the last word found isn't an empty string
         if (word != "") {
-            substrings.push_back(target.substr(start));
+            // Insert quotes before & after last substring found
+            target.insert(start, "\"");
+            target.insert(start + word.size() + 1, "\"");
+            string last_substr = target.substr(start, word.size() + 2);
+            // Add last substring to substrings string vector
+            substrings.push_back(last_substr);
         }
-
-        // Print substrings
-        cout << "The substrings are: ";
-        for (int i = 0; i < substrings.size(); i++) {
-            cout << "\"" << substrings[i] << "\"";
-            // Print out a comma if the index in substrings vector is not the last index of the vector
-            if ((i < substrings.size() - 1)) {
-                cout << ", ";
-            }
-        }
-        cout << endl;
     }
-
-
-    return 0;
+    // Organize substrings with proper formatting and print statement, then return substrings
+    cout << "The substrings are: ";
+    for (int i=0; i < substrings.size(); i++) {
+        cout << substrings[i];
+    }
+    return substrings;
 }
