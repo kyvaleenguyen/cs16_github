65ee22b595ec2c845ad963e4381261d2b75f7173
Kyvalee Nguyen <kyvaleenguyen@ucsb.edu>
2026-02-09 19:41:17 -0800

    fixed more formatting & logic w/ edge cases

--- labs/lab05/vectorSplit.cpp
+++ labs/lab05/vectorSplit.cpp
@@ -36,73 +36,64 @@ vector<string> split(string target, string delimiter) {
 
     // EDGE CASES @-@
 
-    // If target is a single space & delimiter is empty string
-    if (target == " " && delimiter == "") {
-        return substrings;
-    }
-
-    // If delimiter is empty string
+    // If delimiter empty
     if (delimiter == "") {
         return substrings;
     }
 
-    // Print substrings
-    // If delimiter == target
-    if (target == delimiter) {
-        cout << "The substrings are:";
+    // If delimiter in target
+    if (target.find(delimiter) != string::npos) {
+        cout << "The substrings are: ";
+    } else {
+        cout << "No substrings.";
         return substrings;
     }
 
-    // If no delimiter in target
-    if (target.find(delimiter) == string::npos) {
-        cout << "The substrings are: \"" << target << "\"" << endl;
+    // If target == delimite
+    if (target == delimiter) {
         return substrings;
     }
 
     // If there are substrings
-    if (target.find(delimiter) != string::npos) {
-        // Create substrings vector and word string to store substrings in and add to substrings vector
-        // Control size of substrings vector to save memory
-        substrings.reserve(target.size());
-        // Integer to set start of target index for substrings to 0
-        int start = 0;
-        for (int i = 0; i < target.size(); i++) {
-            // If delimiter found at index i in target and if the substr size is greater than 0
-            if ((target[i] == delimiter[0]) && ((i-start)>=0)) {
-                // If the word found is not an empty string, add it to the substrings vector
-                word = target.substr(start, i - start);
-                if (word != "") {
-                    word.insert(0, "\"");
-                    word.insert(word.size(), "\"");
-                    // If word isnt the first element, add comma to last substring
-                    if (substrings.size() > 0) {
-                        substrings.back() += ", ";
-                    }
-                    substrings.push_back(word);
+    // Create substrings vector and word string to store substrings in and add to substrings vector
+    // Control size of substrings vector to save memory
+    substrings.reserve(target.size());
+    // Integer to set start of target index for substrings to 0
+    int start = 0;
+    for (int i = 0; i < target.size(); i++) {
+        // If delimiter found at index i in target and if the substr size is greater than 0
+        if (target[i] == delimiter[0]) {
+            // If the word found is not an empty string, add it to the substrings vector
+            word = target.substr(start, i - start);
+            if (word != "") {
+                word.insert(0, "\"");
+                word.insert(word.size(), "\"");
+                // If word isnt the first element, add comma to last substring
+                if (substrings.size() > 0) {
+                    substrings.back() += ", ";
                 }
-                // Change start index if delimiter found
-                start = i + 1;
+                substrings.push_back(word);
             }
+            // Change start index if delimiter found
+            start = i + 1;
         }
-    
-        // Get last substring after last delimiter & make sure the last word found isn't an empty string
-        if (start < target.size()) {
-            string last_substr = target.substr(start);
-            if (last_substr != "") {
-               string formatted_substr = "\"" + last_substr + "\"";
-               // Add comma before last substring 
-               if (substrings.size() > 0) {
-                   substrings.back() += ", ";
-               }
-               substrings.push_back(formatted_substr); 
+    }
+
+    // Get last substring after last delimiter & make sure the last word found isn't an empty string
+    if (start < target.size()) {
+        string last_substr = target.substr(start);
+        if (last_substr != "") {
+            string formatted_substr = "\"" + last_substr + "\"";
+            // Add comma before last substring 
+            if (substrings.size() > 0) {
+                substrings.back() += ", ";
             }
+            substrings.push_back(formatted_substr); 
         }
-        cout << "The substrings are: ";
-        // Organize substrings with proper formatting and print statement, then return substrings
-        for (int i=0; i < substrings.size(); i++) {
-            cout << substrings[i];
-        }
-        return substrings;
+    }
+    // Organize substrings with proper formatting and print statement, then return substrings
+    for (int i=0; i < substrings.size(); i++) {
+        cout << substrings[i];
     }
     return substrings;
 }
