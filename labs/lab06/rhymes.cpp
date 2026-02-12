9e179fef2c9d0359674844ab2965f03dbfb18be3
Kyvalee Nguyen <kyvaleenguyen@csilvm-07.cs.ucsb.edu>
2026-02-11 23:25:54 -0800

    added rhymes_program to lab06 directory in csil

---
+++ labs/lab06/rhymes.cpp
@@ -0,0 +1,25 @@
+// Include statements go here...
+
+using namespace std;
+
+// FUNCTION DECLARATIONS: YOU MUST DEFINE AND USE THESE (do not remove):
+//      You can declare/define other functions if you like as well,
+//      but you must use the 3 functions below.
+// See lab description for more on these functions.
+
+string findLastWord(string line);
+void cleanUp(string &word);
+bool compareWords(string word1, string word2);
+
+int main() {
+    // MISSING CODE HERE (you can remove these comments)
+    // Create input stream object, then get a filename from user (check it too)
+    // Read the lines in your file and check on rhyming, per our definition
+    // Finally, print the results (see lab descriptions for examples)
+
+    return 0;
+}
+
+// MISSING FUNCTION DEFINITIONS HERE
+// Make sure you have Pre-Conditions and Post-Conditions defined for each function you define here!
+// (you can remove these comments)
