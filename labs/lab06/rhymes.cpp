15081820709a245abe145cc66fdf1f9ef639f140
Kyvalee Nguyen <kyvaleenguyen@ucsb.edu>
2026-02-12 11:05:17 -0800

    edited comments in rhymes.cpp

--- labs/lab06/rhymes.cpp
+++ labs/lab06/rhymes.cpp
@@ -1,4 +1,5 @@
 // Include statements go here...
+#include <fstream>
 
 using namespace std;
 
@@ -12,14 +13,18 @@ void cleanUp(string &word);
 bool compareWords(string word1, string word2);
 
 int main() {
-    // MISSING CODE HERE (you can remove these comments)
     // Create input stream object, then get a filename from user (check it too)
+
+
     // Read the lines in your file and check on rhyming, per our definition
+
+
     // Finally, print the results (see lab descriptions for examples)
+
+    
 
     return 0;
 }
 
 // MISSING FUNCTION DEFINITIONS HERE
 // Make sure you have Pre-Conditions and Post-Conditions defined for each function you define here!
-// (you can remove these comments)
