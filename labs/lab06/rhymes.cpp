4a873521332c664a926316b5c2ee9c30ec20224f
Kyvalee Nguyen <kyvaleenguyen@ucsb.edu>
2026-02-16 16:27:14 -0800

    updating rhymes

--- labs/lab06/rhymes.cpp
+++ labs/lab06/rhymes.cpp
@@ -1,5 +1,6 @@
-// Include statements go here...
 #include <fstream>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -14,22 +15,30 @@ bool compareWords(string word1, string word2);
 
 int main() {
     // Create input stream object, then get a filename from user (check it too)
-    ifstream input_stream;
-    ofstream output_stream;
+    ifstream in_stream;
+    ofstream out_stream;
     string file_name;
 
-    // Get file name from user and open file
-    input_stream << getline(cin, file_name);
-    input_stream.open(file_name);
+    // Get file name from user and open ifstream
+    cout << "Enter file name: ";
+    getline(cin, file_name);
+    in_stream.open(file_name);
+
+    // Get lines from input file
+    in_stream >> 
+
+    // Open ofstream & print get file name line
+    out_stream.open("results.txt");
+    out_stream << "Enter file name: " << file_name << endl;
 
     // If opening file fails
-    if (in.fail()) {
-        cerr << "Cannot open " << file_name << endl; 
-        exit(1); 
+    if (!in_stream.is_open()) {
+        out_stream << "Cannot open " << file_name;
+        return 0;
     }
 
     // Read the lines in your file and check on rhyming, per our definition
-    input_stream << 
+    in_stream << 
 
     // Finally, print the results (see lab descriptions for examples)
     // output_stream << 
@@ -45,7 +54,26 @@ int main() {
 // There MUST already be AT LEAST one word in the string. 
 // Post-condition: The function returns the last word found in the string. If there are no spaces, the whole string is returned
 //      Note: the string MUST be a string (no numbers??)
-string findLastWord(string line){
+string findLastWord(string line) {
+    // Create ifstream to read lines from
+    ifstream in_stream;
+    string line;
+    string last_word;
+    // Iterate through each line in the file
+    while (getline(in_stream, line)) {
+        // Iterate in reverse through each character in the line until a space is found
+        for (int i = line.length(); i > 0; i--) {
+            // If a space is found in the line, make sure that it is not at the beginning or end of the line because it can't be counted as a word
+            if ((line.find(" ")) && (line.find(" ") < line.length()) && (line.find(" ") > 0)) {
+                last_word = line.substr(i, line.length() - i);
+            }
+        }
+
+    }
+
+
+
+
     int words = 1;
     int start = 0;
     for (int i = 0; i < line.size()) {
