236a1c9f654cacaf84b62c2f12382a8c4dfae214
Kyvalee Nguyen <kyvaleenguyen@ucsb.edu>
2026-02-19 14:48:57 -0800

    added function definitions to pfunctions

--- labs/lab07/pfunctions.cpp
+++ labs/lab07/pfunctions.cpp
@@ -1,4 +1,3 @@
-// File: pfunctions.cpp
 #include <iostream>
 #include <string>
 
@@ -6,15 +5,46 @@ using namespace std;
 
 #include "pheaders.h"
 
-// DEFINE THE 2 FUNCTIONS PER THE pheaders.h FILE HERE
-// Add Pre- and Post-Condition comments
-
+// Function call: isPalindrome
 bool isPalindrome(string s) {
-    // This MUST be written as a RECURSIVE FUNCTION! (or you get no lab credit)
-
-    return(true); // This is a STUB - remove it once the function is completed.
+    // Create 2 ints to get first & last position when comparing letters from front to back going inwards
+    int firstLetter = s[0];
+    // REMEMBER: when getting the last character of the string, do string[string.size()-1] because indexing STARTS AT ZERO
+    int lastLetter = s[s.size()-1];
+    // Check if the first & last letters match. If they do, continue inwards checking if the sentence is a palindrome
+    if (s[firstLetter] == s[lastLetter]) {
+        // Check if indexing has stopped by checking if first letter is same index as last letter.
+        // If the recursive function is able to get to this point & go through the whole sentence, the word is a palindrome so return true. 
+        if (firstLetter == lastLetter) { // Base case
+            return true;
+        }
+        // Update position of first & last letter to continue checking inwards
+        firstLetter++;
+        lastLetter--;
+        // Reassign correct indexing to string s once ends of sentence have been verified to be equal to each other & continue checking inwards. RECURSION :D
+        s = s.substr(firstLetter, lastLetter - firstLetter);
+        // Recursive function call
+        isPalindrome(s);
+    } else {
+        // If the letters dont match, the sentence isnt a palindrome. Return false
+        return false;
+    }
 }
 
-void cleanUp(string &s) { 
-    cout << s << endl; // This is a TRACE - remove it once the function is completed. 
+
+// Pre-condition: The string word must already exist and be defined as a string type variable upon function call
+// Post-condition: Lowers capitalization for all words & removes non-letter characters within the string
+void cleanUp(string &s) {
+    // Create string variable to store cleaned string
+    string cleanString = "";
+
+    // Remove non-letter characters & lower letter characters in string
+    for (char c : s) {
+        // Check if character is a letter
+        if(isalpha(c)) {
+            cleanString += tolower(c);
+        }
+    }
+    // Assign cleaned string to word reference
+    s = cleanString;
 }
