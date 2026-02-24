f0eec7d2f203a580c3b76be6c9700bcc9603bd00
Kyvalee Nguyen <kyvaleenguyen@ucsb.edu>
2026-02-23 16:52:42 -0800

    editing hw7

--- hw/hw7/hw7.cpp
+++ hw/hw7/hw7.cpp
@@ -10,8 +10,7 @@ int sumOdds(int N);
 
 int main () {
     // #4
-    int a = 8;
-    cout << sumOdds(a) << endl;
+    cout << sumOdds(8) << endl;
 }
 
 // #4 function definition: Write a function definition of a recursive function that finds the sum of all odd integers in the first int N numbers
