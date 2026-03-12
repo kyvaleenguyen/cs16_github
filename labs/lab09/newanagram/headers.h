2e08f0b35f72ace20bdda007675b3026ab3b82af
Kyvalee Nguyen <kyvaleenguyen@ucsb.edu>
2026-03-11 22:09:04 -0700

    finished program 1 for lab9

--- labs/lab09/newanagram/headers.h
+++ labs/lab09/newanagram/headers.h
@@ -2,8 +2,22 @@
 
 // Define class AString here!
 // See PDF for details
+
+#include <string>
+
+using namespace std;
+
 class AString {
-    // CODE MISSING HERE!
+    public:
+        AString();  // Default constructor
+        AString(string strVal);     // Non default constructor
+        string getStringValue();    // Accessor function
+        void cleanUp();  // Mutator
+        void countLetters(int alphabet[26]);    // Mutator
+        void getAString();  // Mutator
+    private:
+        string StringValue; // Member variable
+
 };
 
 // DO NOTE CHANGE THESE AND DO NOT ADD TO THEM!
